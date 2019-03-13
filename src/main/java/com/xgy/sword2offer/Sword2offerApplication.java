b69ab3fbc86444fa6063898464ba372da9c28192
justxgy <justxgy@outlook.com>
2019-03-13 16:04:45 +0800

    first commit

---
+++ src/main/java/com/xgy/sword2offer/Sword2offerApplication.java
@@ -0,0 +1,13 @@
+package com.xgy.sword2offer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sword2offerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sword2offerApplication.class, args);
+    }
+
+}
